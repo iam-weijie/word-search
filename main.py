9f1902a13e03cc076a13e8b769811f1035525c0e
Weijie Wang <wang.weijie0110@gmail.com>
2024-03-14 20:29:55 -0400

    fix

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import word_search
 
 letter_list = input("Input a list of letters (with no space): ")
 
-words = input("Input the words to search for (use `-` between the words): ")
+words = input("Input the words to search for (use - between the words): ")
 
 print(word_search.word_search_main(letter_list, words))
